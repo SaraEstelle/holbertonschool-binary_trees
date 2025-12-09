e85285707c232d90895820dde901f3e1d1d44bc0
SaraEstelle <12152@holbertonstudents.com>
2025-12-09 10:42:33 +0100

    correcting the comment section on the file

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,10 +1,11 @@
 #include "binary_trees.h"
 /**
- * binary_tree_balance_b - Measures the height of a binary tree for balance
+ * binary_tree_height_b - Measures the height of a binary tree for balance
  * @tree: Pointer to the root node of the tree to measure the height
  *
  * Return: The height of the tree (1-based), or 0 if tree is NULL
  */
+
 size_t binary_tree_height_b(const binary_tree_t *tree)
 {
 	size_t l = 0;
