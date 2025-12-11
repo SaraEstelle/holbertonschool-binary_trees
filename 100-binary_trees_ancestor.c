f490acabd63416c43905d8cbfc8d760f6735c80c
maxim880000 <mdholberton@gmail.com>
2025-12-11 15:48:59 +0100

    adding 100 ancestror

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,14 +1,54 @@
 #include "binary_trees.h"
+
 /**
- * binary_trees_ancestor - finds the lowest common ancestor of two nodes
- * @first: pointer to the first node
- * @second: pointer to the second node
+ * binary_trees_ancestor - trouve l'ancêtre commun le plus bas
+ * @first: premier node
+ * @second: deuxième node
  *
- * Return: If no common ancestor was found, your function must return NULL
- * eturn a pointer to the lowest common ancestor node of the two given nodes
+ * Return: pointeur vers l'ancêtre commun, sinon NULL
  */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
-									const binary_tree_t *second)
+				     const binary_tree_t *second)
 {
-	
+	const binary_tree_t *f, *s;
+
+	if (!first || !second)
+		return (NULL);
+
+	/* on va boucler tant qu'il reste quelque chose à explorer */
+	f = first;
+	s = second;
+
+	while (f || s)
+	{
+		/* comparer tout le chemin de f avec second */
+		if (f)
+		{
+			const binary_tree_t *temp = second;
+
+			while (temp)
+			{
+				if (temp == f)
+					return ((binary_tree_t *)f);
+				temp = temp->parent;
+			}
+			f = f->parent;
+		}
+
+		/* comparer tout le chemin de s avec first */
+		if (s)
+		{
+			const binary_tree_t *temp = first;
+
+			while (temp)
+			{
+				if (temp == s)
+					return ((binary_tree_t *)s);
+				temp = temp->parent;
+			}
+			s = s->parent;
+		}
+	}
+
+	return (NULL);
 }
